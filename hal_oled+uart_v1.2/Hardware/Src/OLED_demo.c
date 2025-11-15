4b28f6103d41aa540bcea1f167678c132485c4f3
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-11-15 15:59:33 +0800

    hal_oled+uart
    添加了串口打印功能

---
+++ hal_oled+uart_v1.2/Hardware/Src/OLED_demo.c
@@ -0,0 +1,44 @@
+#include "OLED.h"
+
+
+void jiemian2(void)//滴速设置界面布局
+{
+	OLED_Clear();
+	
+	OLED_ShowChinese(16,16,"目标滴速设置");
+	OLED_ShowChinese(64,32,"滴");
+	OLED_ShowChar(80,32,'/',OLED_8X16);
+	OLED_ShowChinese(96,32,"分");
+
+	OLED_Update();
+//	currentpage=2;
+	
+
+}
+
+
+
+
+void jiemian1(void)//检测界面布局
+{
+	
+	
+	
+	OLED_Clear();
+
+	OLED_ShowChinese(0,0,"工作状态：");
+	OLED_ShowChinese(0,16,"滴速：");
+	OLED_ShowChinese(90,16,"滴");
+	OLED_ShowChar(106,16,'/',OLED_8X16);
+	OLED_ShowChinese(112,16,"分");
+	OLED_Printf(46,16,OLED_8X16,"%04d",0000);
+	OLED_ShowChinese(0,32,"温度：");
+	OLED_ShowChinese(90,32,"℃");
+	OLED_Printf(46,32,OLED_8X16,"%04.1f",00.0);
+	OLED_ShowChinese(0,48,"药液：");
+	OLED_Update();
+//	currentpage=1;
+	
+	
+
+}
