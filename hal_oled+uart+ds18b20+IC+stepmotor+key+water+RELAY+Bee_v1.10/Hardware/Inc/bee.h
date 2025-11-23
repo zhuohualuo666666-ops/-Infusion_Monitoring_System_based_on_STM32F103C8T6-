6ff1020421b413791aa31e91694d6da7f0bf61cb
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-11-23 18:10:23 +0800

    hal_oled+uart+ds18b20+IC+stepmotor+key+water+RELAY+Bee_v1.10
    继电器控制加热片温度算法

---
+++ hal_oled+uart+ds18b20+IC+stepmotor+key+water+RELAY+Bee_v1.10/Hardware/Inc/bee.h
@@ -0,0 +1,17 @@
+#ifndef __BEE_h
+#define __BEE_h
+
+
+#include "stm32f1xx_hal.h"  // CubeMX 自动生成
+#include <stdint.h>
+
+
+void BEE_OFF(void);
+void BEE_ON(void);
+
+
+
+
+
+
+#endif
---
+++ hal_oled+uart+ds18b20+IC+stepmotor+key+water+RELAY+Bee_v1.10/Hardware/Src/bee.c
@@ -0,0 +1,16 @@
+#include "bee.h"
+#include "gpio.h"
+
+void BEE_ON(void)
+{
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,GPIO_PIN_SET);
+}
+
+
+
+void BEE_OFF(void)
+{
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_9,GPIO_PIN_RESET);
+}
+
+
